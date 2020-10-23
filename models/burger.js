fa2da442b55139b33249e41608fa70052bf10e95
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-10-23 02:41:53 -0700

    burger model

---
+++ models/burger.js
@@ -0,0 +1,16 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+
+    },
+    insertOne: function(cols, vals, cb) {
+
+    },
+    updateONE: function(objColVals, condition, cb) {
+
+    }
+    
+};
+
+module.exports = burger;
