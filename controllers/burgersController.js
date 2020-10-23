931e12e7d3ba59f6aa0e71edd03073644ffdf7ea
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-10-23 02:50:52 -0700

    burgers controller

---
+++ controllers/burgersController.js
@@ -0,0 +1,40 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req,res){
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.logg(hbsObject);
+    });
+});
+
+router.post("/api/burgers",function(req,res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+        res.json({ id: result.insertId })
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.updateONE(
+        {
+            sleepy:body.devoured
+        },
+        condition,
+        function(result) {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+module.exports = router;
