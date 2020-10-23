b7596c425e0d451a63850f4eb55c8026c7b66953
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-10-23 02:29:04 -0700

    seed

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
