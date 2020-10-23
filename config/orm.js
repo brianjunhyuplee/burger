b271ad98fccec4daa22f40ab0ae66f0f43335b4a
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-10-23 02:38:45 -0700

    orm basic template

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+var connection = require("../config/connection.js");
+
+var orm ={
+selectAll : function(tableInput, cb) {
+
+},
+insertOne : function(table, cols, vals, cb) {
+
+},
+updateOne : function(table, objColVals, condition, cb) {
+
+}
+
+
+};
+
+module.exports = orm;
